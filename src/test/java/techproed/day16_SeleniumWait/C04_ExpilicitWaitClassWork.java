ae43f92417f9bfec3c829f156397401078f130ef
Zulfiye <zulfiyezulfiyek@gmail.com>
2023-06-21 00:14:47 +0300

    day16,C04

--- src/test/java/techproed/day16_SeleniumWait/C04_ExpilicitWaitClassWork.java
+++ src/test/java/techproed/day16_SeleniumWait/C04_ExpilicitWaitClassWork.java
@@ -1,2 +1,31 @@
-package techproed.day16_SeleniumWait;public class C04_ExpilicitWaitClassWork {
+package techproed.day16_SeleniumWait;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+public class C04_ExpilicitWaitClassWork extends TestBase {
+    @Test
+    public void test1() {
+
+        //https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver adresine gidelim
+        driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+
+
+        //Click me to open an Alert buttonuna basalım
+         driver.findElement(By.xpath("//button[@id='alert']")).click();
+
+         new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.alertIsPresent());
+         //alert cikana kadar bekleyecek.
+
+
+        //Çıkan Alert'ü kapatalım
+        alertAccept();
+
+
+    }
 }
